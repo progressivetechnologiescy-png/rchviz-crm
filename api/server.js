7bb5e273a736fbbba893cdb1248f4ee65d1ba60b
koss <koss@Georges-MacBook-Air.local>
2026-04-17 10:39:52 +0300

    Fix Express 100kb payload limit to support base64 image uploads

--- api/server.js
+++ api/server.js
@@ -8,7 +8,8 @@ const axios = require('axios');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 const authRoutes = require('./routes/auth');
